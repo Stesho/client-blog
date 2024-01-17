27c12e2d3bb08827815cd0a94b5019dc5a7302c3
Steshenko Dmitry <3301297@gmail.com>
2024-01-17 18:51:23 +0300

    feat: add blog head

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Join } from '@/components/ui/Join/Join';
+import { BlogHead } from '@/components/ui/BlogHead/BlogHead';
 
 export default function Page() {
   return (
     <main>
+      <BlogHead />
       <Join />
     </main>
   );
