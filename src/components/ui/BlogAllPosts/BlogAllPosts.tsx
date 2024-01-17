c256365aff433fc6a7c920037e5523d02b480c0e
Steshenko Dmitry <3301297@gmail.com>
2024-01-17 20:10:29 +0300

    feat: add all posts controllers

--- src/components/ui/BlogAllPosts/BlogAllPosts.tsx
+++ src/components/ui/BlogAllPosts/BlogAllPosts.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { BlogPostCard } from '@/components/ui/BlogPostCard/BlogPostCard';
 import { POSTS } from '@/constants/posts';
 import styles from './BlogAllPosts.module.scss';
 
-export const BlogAllPosts = () => (
-    <section className="section container">
+export const BlogAllPosts = () => {
+  const [page, setPage] = useState(0);
+  const displayItemsCount = 5;
+
+  const onNext = () => setPage((prev) => prev + 1);
+  const onPrev = () => setPage((prev) => prev - 1);
+
+  const isPrevDisabled = () => page === 0;
+  const isNextDisabled = () =>
+    page + 1 === Math.ceil(POSTS.length / displayItemsCount);
+
+  return (
+    <section className='section container'>
       <h2 className={`${styles.header} heading1`}>All posts</h2>
       <ul className={styles.list}>
-        {POSTS.map((post) => (
+        {POSTS.slice(
+          page * displayItemsCount,
+          page * displayItemsCount + displayItemsCount,
+        ).map((post) => (
           <li key={post.id}>
             <BlogPostCard
               imageURL={post.image}
@@ -20,12 +36,23 @@ export const BlogAllPosts = () => (
         ))}
       </ul>
       <div className={styles.buttons}>
-        <button type='button' className={`${styles.controller} heading3`}>
+        <button
+          disabled={isPrevDisabled()}
+          onClick={onPrev}
+          type='button'
+          className={`${styles.controller} heading3`}
+        >
           {'<'} Prev
         </button>
-        <button type='button' className={`${styles.controller} heading3`}>
+        <button
+          disabled={isNextDisabled()}
+          onClick={onNext}
+          type='button'
+          className={`${styles.controller} heading3`}
+        >
           Next {'>'}
         </button>
       </div>
     </section>
   );
+};
