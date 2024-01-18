a6c95e1c32702152adb6377685bdaca059a8d99f
Steshenko Dmitry <3301297@gmail.com>
2024-01-18 20:26:09 +0300

    fix: set correct head styles on blog post page

--- src/components/ui/BlogPostHead/BlogPostHead.tsx
+++ src/components/ui/BlogPostHead/BlogPostHead.tsx
@@ -5,20 +5,22 @@ import Shuttle from '@/assets/img/category/shuttle.png';
 import styles from './BlogPostHead.module.scss';
 
 export const BlogPostHead = () => (
-    <section className={`${styles.section} topSection`}>
-      <div className={styles.author}>
-        <Image src={Author} alt="author" />
-        <div>
-          <span>Andrew Jonson</span>
-          <span>Posted on 27th January 2022</span>
-        </div>
-      </div>
-      <h1 className="heading1">
-        Step-by-step guide to choosing great font pairs
-      </h1>
+  <section className={`${styles.section} topSection`}>
+    <div className={styles.author}>
+      <Image className={styles.authorImg} src={Author} alt='author' />
       <div>
-        <Image src={Shuttle} alt='shuttle' />
-        <span className="heading4">Startup</span>
+        <span className={`${styles.authorName} heading3`}>Andrew Jonson</span>
+        <span className={`${styles.postedDate} body1`}>
+          Posted on 27th January 2022
+        </span>
       </div>
-    </section>
-  );
+    </div>
+    <h1 className='heading1'>
+      Step-by-step guide to choosing great font pairs
+    </h1>
+    <div className={styles.type}>
+      <Image src={Shuttle} alt='shuttle' />
+      <span className='heading4'>Startup</span>
+    </div>
+  </section>
+);
--- src/components/ui/WhatToRead/WhatToRead.tsx
+++ src/components/ui/WhatToRead/WhatToRead.tsx
@@ -4,21 +4,21 @@ import { BlogCard } from '@/components/ui/BlogCard/BlogCard';
 import styles from './WhatToRead.module.scss';
 
 export const WhatToRead = () => (
-    <section className="section container">
-      <h2 className="heading2">What to read next</h2>
-      <ul className={styles.list}>
-        {POSTS.slice(0, 3).map((post) => (
-          <li key={post.id}>
-            <BlogCard
-              alt={post.title}
-              author={post.author}
-              date={post.date}
-              imageURL={post.image}
-              text={post.text}
-              title={post.title}
-            />
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+  <section className='section container'>
+    <h2 className={`${styles.header} heading2`}>What to read next</h2>
+    <ul className={styles.list}>
+      {POSTS.slice(0, 3).map((post) => (
+        <li key={post.id}>
+          <BlogCard
+            alt={post.title}
+            author={post.author}
+            date={post.date}
+            imageURL={post.image}
+            text={post.text}
+            title={post.title}
+          />
+        </li>
+      ))}
+    </ul>
+  </section>
+);
