101f7b3bbd292c7e5ba2779e4060768d65536593
Steshenko Dmitry <3301297@gmail.com>
2024-02-01 07:03:47 +0300

    feat: add translations for author page

--- src/components/ui/AuthorProfile/AuthorProfile.tsx
+++ src/components/ui/AuthorProfile/AuthorProfile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 
 import { FOOTER_SOCIAL } from '@/constants/footer';
 import { Author } from '@/types/author';
@@ -10,34 +11,31 @@ interface AuthorProfileProps {
   author: Author;
 }
 
-export const AuthorProfile = ({ author }: AuthorProfileProps) => (
-  <section className={`${styles.section} section topSection`}>
-    <div className={`${styles.wrapper} container`}>
-      <Image
-        className={`${styles.avatar}`}
-        src={author.imageURL}
-        alt={author.name}
-      />
-      <div className={styles.authorInfo}>
-        <h1 className='heading1'>
-          Hey there, I’m {author.name} and welcome to my Blog
-        </h1>
-        <p className='body1'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Non
-          blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At
-          risus viverra adipiscing at in tellus.
-        </p>
-        <ul className={styles.links}>
-          {FOOTER_SOCIAL.map((link) => (
-            <li>
-              <a href={link.link}>
-                <Image src={link.iconURL} alt={link.alt} />
-              </a>
-            </li>
-          ))}
-        </ul>
+export const AuthorProfile = ({ author }: AuthorProfileProps) => {
+  const t = useTranslations('author');
+
+  return (
+    <section className={`${styles.section} section topSection`}>
+      <div className={`${styles.wrapper} container`}>
+        <Image
+          className={`${styles.avatar}`}
+          src={author.imageURL}
+          alt={author.name}
+        />
+        <div className={styles.authorInfo}>
+          <h1 className='heading1'>{t('title', { name: author.name })}</h1>
+          <p className='body1'>{t('text')}</p>
+          <ul className={styles.links}>
+            {FOOTER_SOCIAL.map((link) => (
+              <li>
+                <a href={link.link}>
+                  <Image src={link.iconURL} alt={link.alt} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
